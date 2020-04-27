2c14bdee7828e9890716ba253c193425a9c540bb
Ernesto <ernesto.cubanportrait@gmail.com>
2020-04-27 07:54:01 -0400

    Updates

---
+++ precache-manifest.fad16fb6f07059a670562229a9081b85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd16d00a88fcaf9512a6e1e444d622dc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "4b34160fe86e307ccce0",
+    "url": "/my-app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "67216a8ba05a8a09415e",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b34160fe86e307ccce0",
+    "url": "/my-app/static/js/2.58594fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.58594fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67216a8ba05a8a09415e",
+    "url": "/my-app/static/js/main.5593533d.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  }
+]);
